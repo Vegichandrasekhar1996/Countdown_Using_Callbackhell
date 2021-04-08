bfc4475fd5f2b81182f14d2193bae4b24669560b
Vegichandrasekhar1996 <79332126+Vegichandrasekhar1996@users.noreply.github.com>
2021-04-09 00:35:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+let count = 10;
+setTimeout(() => {
+    document.getElementById("div").innerHTML = count;
+    setTimeout(() => {
+        document.getElementById("div").innerHTML = count-1,count--;
+        setTimeout(() => {
+            document.getElementById("div").innerHTML = count-1,count--;
+            setTimeout(() => {
+                document.getElementById("div").innerHTML = count-1,count--;
+                setTimeout(() => {
+                    document.getElementById("div").innerHTML = count-1,count--;
+                    setTimeout(() => {
+                        document.getElementById("div").innerHTML = count-1,count--;
+                        setTimeout(() => {
+                            document.getElementById("div").innerHTML = count-1,count--;
+                            setTimeout(() => {
+                                document.getElementById("div").innerHTML = count-1,count--;
+                                setTimeout(() => {
+                                    document.getElementById("div").innerHTML = count-1,count--;
+                                    setTimeout(() => {
+                                        document.getElementById("div").innerHTML = count-1,count--;
+                                        setTimeout(() => {
+                                            document.getElementById("div").innerHTML = "Happy independence Day";                           
+                                        }, 1000);                           
+                                    }, 1000);                               
+                                }, 1000);                    
+                            }, 1000);                            
+                    }, 1000);                  
+                }, 1000);                   
+            }, 1000);
+            
+    
+            }, 1000);
+
+    
+        }, 1000);
+    
+    }, 1000);
+    
+    
+}, 1000);
